8bb6fdb425fbe33c6aeed2f6ad6ae89fee2cf768
konstantined <kdolgan@yandex.ru>
2012-10-14 20:51:53 +0300

    Tests added

--- test/1.pl
+++ test/1.pl
@@ -2,10 +2,19 @@
 
 use strict;
 use Data::Dumper;
+ use Test::More tests => 2;
 
-my $x = 'AAAAA';
+use Util;
 
-print "x: $x\n";
+my ($x, $y);
+
+$x = sub1();
+print "sub1: $x\n";
+$y = sub2();
+print "sub2: $y\n";
+
+ok($x eq '1', 'xxxxxxxxx');
+ok($y eq '1', 'yyyyyyyyy');
 print "\n------------\n\n";
 
 #
